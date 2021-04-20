4ee5bd012bee478cba1d77ef8936580da2a3cedf
Panagiotis H.M. Issaris <takis@issaris.com>
2021-04-20 11:42:44 +0200

    Reformat using black

--- odoobrowser.py
+++ odoobrowser.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 cache = SimpleCache(default_timeout=1)
 
 
-SERVER = os.getenv('ODOO_SERVER', 'http://127.0.0.1:8069')
-DATABASE = os.getenv('ODOO_DB', 'odoodb')
-USERNAME = os.getenv('ODOO_USERNAME', 'admin')
-PASSWORD = os.getenv('ODOO_PASSWORD', 'admin')
+SERVER = os.getenv("ODOO_SERVER", "http://127.0.0.1:8069")
+DATABASE = os.getenv("ODOO_DB", "odoodb")
+USERNAME = os.getenv("ODOO_USERNAME", "admin")
+PASSWORD = os.getenv("ODOO_PASSWORD", "admin")
 CONFIG = dict(
     server=SERVER,
     database=DATABASE,
@@ -28,19 +28,19 @@ def query_odoo(model, operation, param, opts=None):
     :param opts: the options for the action
     :returns: the Odoo results
     """
-    app.logger.debug('%s %s %s', SERVER, DATABASE, USERNAME)
+    app.logger.debug("%s %s %s", SERVER, DATABASE, USERNAME)
 
     # Check if the result was already retrieved earlier
-    key = f'{model}-{operation}-{param}-{opts}'
+    key = f"{model}-{operation}-{param}-{opts}"
     rv = cache.get(key)
     if rv:
-        app.logger.debug(f'returning cached results for {key}')
+        app.logger.debug(f"returning cached results for {key}")
         return rv
     else:
-        app.logger.debug(f'no cached results for {key}')
+        app.logger.debug(f"no cached results for {key}")
 
-    common = ServerProxy(f'{SERVER}/xmlrpc/2/common')
-    models = ServerProxy(f'{SERVER}/xmlrpc/2/object')
+    common = ServerProxy(f"{SERVER}/xmlrpc/2/common")
+    models = ServerProxy(f"{SERVER}/xmlrpc/2/object")
 
     user_id = common.login(DATABASE, USERNAME, PASSWORD)
     try:
@@ -51,11 +51,12 @@ def query_odoo(model, operation, param, opts=None):
             model,
             operation,
             [param],
-            {} if opts is None else opts)
+            {} if opts is None else opts,
+        )
         cache.set(key, results, timeout=5 * 60)
         return results
     except Fault as err:
-        app.logger.debug('Error handling %s', err)
+        app.logger.debug("Error handling %s", err)
 
 
 def get_fields(model_id):
@@ -65,10 +66,7 @@ def get_fields(model_id):
     :return: the fields of the model
     """
     return query_odoo(
-        'ir.model.fields',
-        'search_read',
-        [('model_id', '=', model_id)],
-        {}
+        "ir.model.fields", "search_read", [("model_id", "=", model_id)], {}
     )
 
 
@@ -86,13 +84,13 @@ def create_model_query(model_names):
      >>> create_model_query(['bus.bus', 'edi.edit', 'ir.cron'])
      ['|', ('model', '=', 'bus.bus'), '|', ('model', '=', 'edi.edit'), ('model', '=', 'ir.cron')]
     """
-    query = [('model', '=', item) for item in model_names]
+    query = [("model", "=", item) for item in model_names]
     if len(model_names) < 2:
         return query
 
     combined_query = []
     for item in query[:-1]:
-        combined_query.extend(['|', item])
+        combined_query.extend(["|", item])
     combined_query.append(query[-1])
     return combined_query
 
@@ -105,7 +103,7 @@ def get_models(model_names):
     """
     query = create_model_query(model_names)
     app.logger.debug(query)
-    return query_odoo('ir.model', 'search_read', query)
+    return query_odoo("ir.model", "search_read", query)
 
 
 def get_models_with_relations(model_names):
@@ -115,80 +113,76 @@ def get_models_with_relations(model_names):
     relations = []
     models = []
     for model in results:
-        fields = get_fields(model['id'])
+        fields = get_fields(model["id"])
         models.append((model, fields))
         for field in fields:
-            if 'relation' in field and field['relation']:
-                if field['relation'] in model_names:
+            if "relation" in field and field["relation"]:
+                if field["relation"] in model_names:
                     relations.append(field)
 
     return models, relations
 
 
-@app.route('/')
+@app.route("/")
 def main():
     """Show the main webpage"""
-    return render_template('main.html', config=CONFIG)
+    return render_template("main.html", config=CONFIG)
 
 
-@app.route('/delete/<model_name>/<int:row_id>')
+@app.route("/delete/<model_name>/<int:row_id>")
 def delete_row(model_name, row_id):
     """Delete a record within an Odoo model"""
-    app.logger.debug(f'{model_name}')
-    results = query_odoo(model_name, 'unlink', [row_id])
+    app.logger.debug(f"{model_name}")
+    results = query_odoo(model_name, "unlink", [row_id])
     app.logger.error(results)
-    return redirect(url_for('view_model', name=model_name))
+    return redirect(url_for("view_model", name=model_name))
 
 
-@app.route('/model/<model_name>')
+@app.route("/model/<model_name>")
 def view_model(model_name=None):
     """Show meta info about the records in an Odoo model"""
     opts = dict(
         fields=[
-            'name',
-            'create_uid',
-            'create_date',
-            'write_uid',
-            'write_date',
-            'model_name'
+            "name",
+            "create_uid",
+            "create_date",
+            "write_uid",
+            "write_date",
+            "model_name",
         ],
-        limit=10
+        limit=10,
     )
-    app.logger.debug(f'{model_name}')
-    results = query_odoo(model_name, 'search_read', [], opts)
-    return render_template('data_list.html',
-                           objects=results, model_name=model_name)
+    app.logger.debug(f"{model_name}")
+    results = query_odoo(model_name, "search_read", [], opts)
+    return render_template("data_list.html", objects=results, model_name=model_name)
 
 
-@app.route('/list/')
+@app.route("/list/")
 def list_models():
     """List all Odoo models"""
-    results = query_odoo('ir.model', 'search_read', [])
-    return render_template('model_list.html',
-                           objects=results, length=len(results))
+    results = query_odoo("ir.model", "search_read", [])
+    return render_template("model_list.html", objects=results, length=len(results))
 
 
-@app.route('/fields/<int:model_id>')
+@app.route("/fields/<int:model_id>")
 def list_fields(model_id):
     """List all the fields within a model"""
-    query = [('model_id', '=', model_id)]
+    query = [("model_id", "=", model_id)]
     app.logger.debug(query)
-    results = query_odoo('ir.model.fields', 'search_read', query)
+    results = query_odoo("ir.model.fields", "search_read", query)
     app.logger.debug(results)
-    return render_template('field_list.html',
-                           objects=results, length=len(results))
+    return render_template("field_list.html", objects=results, length=len(results))
 
 
-@app.route('/data/<model_name>')
+@app.route("/data/<model_name>")
 def view_data(model_name):
     """Show the data within a model"""
-    results = query_odoo(model_name, 'search_read', [])
+    results = query_odoo(model_name, "search_read", [])
     app.logger.debug(results)
-    return render_template('all_data_list.html',
-                           objects=results, length=len(results))
+    return render_template("all_data_list.html", objects=results, length=len(results))
 
 
-@app.route('/detail/<model_name>/<int:row_id>')
+@app.route("/detail/<model_name>/<int:row_id>")
 def view_details(model_name, row_id):
     """Show the data of the specific row of a model
 
@@ -196,30 +190,32 @@ def view_details(model_name, row_id):
     :param row_id: the ID of the record in the model
     :return: a page showing the record information
     """
-    query = [('id', '=', row_id)]
+    query = [("id", "=", row_id)]
     models, relations = get_models_with_relations([model_name])
-    results = query_odoo(model_name, 'search_read', query)
+    results = query_odoo(model_name, "search_read", query)
     app.logger.debug(results)
     return render_template(
-        'detail.html',
+        "detail.html",
         object=results[0],
         model=models[0][0],
         fields=models[0][1],
-        relations=relations)
+        relations=relations,
+    )
 
 
-@app.route('/plantuml', methods=['POST'])
+@app.route("/plantuml", methods=["POST"])
 def view_plantuml():
     """Return a PlantUML file for graphing the models"""
     model_names = []
     for k, v in request.form.items():
-        if v == 'on':
+        if v == "on":
             model_names.append(k)
     results, relations = get_models_with_relations(model_names)
-    return render_template(
-        'plantuml.txt',
-        objects=results,
-        relations=relations), 200, {'Content-Type': 'text/plain'}
+    return (
+        render_template("plantuml.txt", objects=results, relations=relations),
+        200,
+        {"Content-Type": "text/plain"},
+    )
 
 
 if __name__ == "__main__":
